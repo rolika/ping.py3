85a6a96ace4d66f85fffd317f806c05fb25dce49
rolika <weisz.roland@pohlen-dach.hu>
2015-08-18 22:05:11 +0200

    coded slider handling

--- ping.py
+++ ping.py
@@ -70,29 +70,51 @@ class Slider(Scale):
     def __init__(self, ro, co, root = None):
         """ Inits itself as a slider with it's grid coords """        
         self.root = root
-        super().__init__(self.root, length = 200, from_ = 1.0, to = 20.0,
-                         command = None)
-        self.grid(row = ro, column = co)
+        super().__init__(self.root, length = 200, from_ = 1.0, to = 16.0,
+                         command = self.root.redrawField) #reference to parent
+        self.grid(row = ro, column = co, sticky = NW)
 
     def setHorizontal(self):
         """ Describes a horizontal slider """
-        self.["orient"], self.["cursor"] = HORIZONTAL, "sb_h_double_arrow"
+        self["orient"], self["cursor"] = HORIZONTAL, "sb_h_double_arrow"
+        self.bind("<MouseWheel>", self.mouseWheelH) #Windows, MacOS
+        self.bind("<Button-4>", self.mouseWheelH) #Linux ,scroll down
+        self.bind("<Button-5>", self.mouseWheelH) #Linux ,scroll up
 
     def setVertical(self):
         """ Describes a horizontal slider """
-        self.["orient"], self.["cursor"] = VERTICAL, "sb_v_double_arrow"
+        self["orient"], self["cursor"] = VERTICAL, "sb_v_double_arrow"
+        self.bind("<MouseWheel>", self.mouseWheelV) #Windows, MacOS
+        self.bind("<Button-4>", self.mouseWheelV) #Linux ,scroll down
+        self.bind("<Button-5>", self.mouseWheelV) #Linux ,scroll up
+    
+    def mouseWheelH(self, event):
+        """ Handles scroll wheel over horizontal slider """
+        self.set(self.get() + int(event.delta / 120)) #Windows,MacOS
+        if event.num == 4: self.set(self.get() + 1) #Linux
+        elif event.num == 5: self.set(self.get() - 1)
+        
+    def mouseWheelV(self, event):
+        """ Handles scroll wheel over vertical slider """
+        self.set(self.get() - int(event.delta / 120)) #Windows,MacOS
+        if event.num == 4: self.set(self.get() - 1) #Linux
+        elif event.num == 5: self.set(self.get() + 1)
         
 class Table(Canvas):
     """ Draws game field """
 
-    def __init__ (self, root = None, columns = 5, rows = 5, disc_size = 50):
+    def __init__ (self, ro, co, root = None, columns = 4, rows = 4,
+                  disc_size = 40):
         """ Game field is drawn upon: rows, columns, disc-size """
-        self.root, self.columns, self.rows, self.disc_size = root, columns, rows, disc_size
-        self.wid, self.hei = self.columns*disc_size, self.rows*disc_size
-        super().__init__(self.root, width = self.wid, height = self.hei, bg = "ivory")
-        self.grid()
-        self.bind("<Button-1>", self.root.flipDisc)
-        self.drawGrid()
+        self.root, self.columns, self.rows, self.disc_size =\
+            root, columns, rows, disc_size
+        self.canvas_width = self.columns * self.disc_size
+        self.canvas_height = self.rows * self.disc_size
+        super().__init__(self.root, width = self.canvas_width,
+                         height = self.canvas_height, bg = "ivory")
+        self.grid(row = ro, column = co, sticky = NW)
+        #self.bind("<Button-1>", self.root.flipDisc)
+        #self.drawGrid()
 
     def drawGrid(self):
         """ Draws grid on game field """
@@ -121,35 +143,38 @@ class Ping(Frame):
         """ Class initialiser """
         super().__init__(root)
         self.grid(row = 0, column = 0)
+        self.disc_size = 40 #diameter
         self.setSliders()
+        self.redrawField(1) #argument because of event handling
+    
+    def setTable(self):
+        """ Draws game table """
+        self.table = Table(2, 1, self, 10, 8, 40)
     
     def setSliders(self):
         """ Sets horizontal and vertical sliders """
-        Label(self, text = "1").grid(row = 1, column = 0) #common start value
+        Label(self, text = "1").grid(row = 1, column = 0, sticky = SE)
         # horizontal slider
-        self.horizontal = Scale(self, orient = HORIZONTAL, length = 200,
-            cursor = "sb_h_double_arrow", showvalue = 1,
-            from_ = 1.0, to = 20.0, command = None)
-        self.horizontal.grid(row = 1, column = 1)
-        Label(self, text = "20").grid(row = 1, column = 2)
+        self.horizontal = Slider(1, 1, self)
+        self.horizontal.setHorizontal()
+        Label(self, text = "20").grid(row = 1, column = 2, sticky = SW)
         self.horizontal.set(4)
-        self.horizontal.bind("<MouseWheel>", self.mouseWheelH)
         # vertical slider
-        self.vertical = Scale(self, orient = VERTICAL, length = 200,
-            cursor = "sb_v_double_arrow", showvalue = 1,
-            from_ = 1.0, to = 20.0, command = None)
-        self.vertical.grid(row = 2, column = 0)
-        Label(self, text = "20").grid(row = 3, column = 0)
+        self.vertical = Slider(2, 0, self)
+        self.vertical.setVertical()
+        Label(self, text = "20").grid(row = 3, column = 0, sticky = NE)
         self.vertical.set(4)
-        self.vertical.bind("<MouseWheel>", self.mouseWheelV)
 
-    def mouseWheelH(self, event):
-        """ Handles scroll wheel over horizontal slider """
-        self.horizontal.set(self.horizontal.get() + int(event.delta / 120))
-        
-    def mouseWheelV(self, event):
-        """ Handles scroll wheel over vertical slider """
-        self.vertical.set(self.vertical.get() - int(event.delta / 120))
+    def redrawField(self, event):
+        """ Redraws game field according to sliders """
+        try:
+            self.table.grid_forget()
+        except:
+            pass
+        self.table = Table(2, 1, self, self.horizontal.get(),
+                           self.vertical.get())
+        self.horizontal["length"] = self.horizontal.get() * self.disc_size
+        self.vertical["length"] = self.vertical.get() * self.disc_size
         
 if __name__ == "__main__":
     Ping().mainloop()
